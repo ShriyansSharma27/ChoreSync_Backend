d71dfffeb02d7e1a861f078164bab29c9335f370
Shriyanssharma27 <shars199@mcmaster.ca>
2026-02-09 15:34:13 -0500

    dual entry during provider signup and RBAC gatekeeper

--- routes/auth.js
+++ routes/auth.js
@@ -49,6 +49,10 @@ auth_router.post('/provider/signup', ClerkMiddleAuth, async (req, res) => {
       await db.query("INSERT INTO Service_providers (first_name, last_name, email) values ($1,$2,$3)", 
         [first_name, last_name, email]
       );
+
+      await db.query("INSERT INTO Customer ( first_name, last_name, email) VALUES ($1, $2, $3);", 
+        [first_name, last_name, email]
+      ); 
       
       return res.sendStatus(201);
     } 
@@ -59,5 +63,35 @@ auth_router.post('/provider/signup', ClerkMiddleAuth, async (req, res) => {
       return res.sendStatus(500);
     }
 });
+
+// Check if email is a provider
+auth_router.get('/isprovider', ClerkMiddleAuth, async (req, res) => {
+  try {
+    const {first_name, last_name, email} = req.params;
+    const clerkId = req.clerkId;
+    
+    if(!first_name || !last_name || !email) return res.sendStatus(400);
+
+    const checkExist = await db.query("SELECT * FROM Service_providers WHERE email = $1", [email]);
+    if(checkExist.rows.length > 0) {
+      return res.status(200).json({
+        "message": "user exists"
+      });
+    }
+
+    if (checkExist.rows.length > 0) {
+      res.json({ isProvider: true });
+    } else {
+      res.json({ isProvider: false });
+    } 
+  } 
+  catch (err) {
+    if(err.code === '23505') {
+      return res.sendStatus(409);
+    }
+    return res.sendStatus(500);
+  }
+});
+
 
 module.exports = auth_router;
